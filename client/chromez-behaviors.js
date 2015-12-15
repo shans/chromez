6e5aa16e5253f99517c70e5abd3dfd9cdef4b53f
Shane Stephens <shanestephens@google.com>
2015-12-15 16:05:30 +1100

    Refactor to use behavior for AJAXy stuff.

---
+++ client/chromez-behaviors.js
@@ -0,0 +1,34 @@
+window.ChromeZBehaviors = window.ChromeZBehaviors || {};
+ChromeZBehaviors.AJAXBehavior = {
+  update: function() {
+    for (var i = 0; i < this.searchQueries.length; i++) {
+      this.searchQueries[i].element.generateRequest();
+    }
+  },
+  addCallbackToQuery: function(query, key, callback, defaults) {
+    for (var i = 0; i < this.searchQueries.length; i++) {
+      if (this.searchQueries[i][key] == query[key]) {
+        this.searchQueries[i].callbacks.push(callback);
+        if (this.searchQueries[i].dataCache)
+          callback(this.searchQueries[i].dataCache);
+        return true;
+      }
+    }
+    searchQuery = {callbacks: [callback], params: defaults};
+    searchQuery[key] = query[key];
+    this.push('searchQueries', searchQuery);
+  },
+  properties: {
+    searchQueries: {
+      type: Object,
+      value: function() { return []; }
+    }
+  },
+  handleResponse: function(data) {
+    var query = this.searchQueries[data.model.index];
+    query.dataCache = data.detail.response;
+    query.element = data.target;
+    for (var i = 0; i < query.callbacks.length; i++)
+      query.callbacks[i](data.detail.response);
+  },
+}
