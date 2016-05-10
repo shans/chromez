446f318aa5b384b832d495129cf67c73ef76bccd
Douglas Stockwell <dstockwell@chromium.org>
2016-05-10 11:27:34 +1000

    Keep system awake always

--- app/main.js
+++ app/main.js
@@ -1,8 +1,1 @@
-setInterval(() => {
-  var a = new Date();
-  if (a.getDay() < 6 && a.getHours() > 6 && a.getHours() < 20) {
-    chrome.power.requestKeepAwake("display");
-  } else {
-    chrome.power.releaseKeepAwake();
-  }
-}, 60 * 1000);
+chrome.power.requestKeepAwake("display");
