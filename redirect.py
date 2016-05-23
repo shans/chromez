7e9a13b9298a0d0887e5359504fd046b5b6596e1
Douglas Stockwell <dstockwell@chromium.org>
2016-05-23 15:01:52 +1000

    Add a redirect for issues().get

--- redirect.py
+++ redirect.py
@@ -9,25 +9,29 @@ import json
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+      scope = 'https://www.googleapis.com/auth/userinfo.email'
+      credentials = AppAssertionCredentials(scope)
+      http = credentials.authorize(Http())
+
+      DISCOVERY_URL = (
+        'https://monorail-prod.appspot.com/_ah/api/discovery/v1/apis/'
+        '{api}/{apiVersion}/rest'
+      )
+
+      monorail = build(
+        'monorail', 'v1',
+        discoveryServiceUrl=DISCOVERY_URL,
+        http=http
+      )
       if self.request.get('site') == 'issues':
-          scope = 'https://www.googleapis.com/auth/userinfo.email'
-          credentials = AppAssertionCredentials(scope)
-          http = credentials.authorize(Http())
-
-          DISCOVERY_URL = (
-            'https://monorail-prod.appspot.com/_ah/api/discovery/v1/apis/'
-            '{api}/{apiVersion}/rest'
-          )
-
-          monorail = build(
-            'monorail', 'v1',
-            discoveryServiceUrl=DISCOVERY_URL,
-            http=http
-          )
-
           urlfetch.set_default_fetch_deadline(10)
           self.response.headers.add_header("Access-Control-Allow-Origin", "*")
           result = monorail.issues().list(projectId='chromium', q=self.request.get('q'), can='open').execute()
+          self.response.write(json.dumps(result))
+      elif self.request.get('site') == 'issue':
+          urlfetch.set_default_fetch_deadline(10)
+          self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+          result = monorail.issues().get(projectId='chromium', issueId=self.request.get('issueId')).execute()
           self.response.write(json.dumps(result))
 
 app = webapp2.WSGIApplication([
