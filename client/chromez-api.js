8302a0a27d59ee9535fc34250544e4b5ac440772
Shane Stephens <shanestephens@google.com>
2016-06-06 14:57:01 +1000

    Remove muliple card dashes by adding affordance for keyed configuration to configs.

--- client/chromez-api.js
+++ client/chromez-api.js
@@ -24,3 +24,30 @@ function removeCard(card) {
   if (cardview)
     return Polymer.Base.async(function() { cardview.removeCard(card); });
 }
+
+function addDash(dashSpec, elementAfter) {
+  if (typeof dashSpec == "object") {
+    var dash = dashSpec.name;
+    var key = dashSpec.key;
+  } else if (dashSpec.indexOf("(") > -1) {
+    var bits = dashSpec.split("(");
+    var dash = bits[0];
+    var key = bits[1].split(")")[0];
+  } else {
+    var dash = dashSpec;
+    var key = undefined;
+  }
+
+  var e = elementAfter.parentElement.querySelector(dash);
+  if (e && e.key == key)
+    return;
+  Polymer.Base.importHref(dash + '.html',
+    function(e) {
+      var elt = document.createElement(dash);
+      elt.key = key;
+      elementAfter.parentElement.insertBefore(elt, elementAfter);
+    },
+    function(e) {
+      console.error("couldn't create dash " + dash);
+    });
+}
