7c6a31fd468802e1a3018cd1bd86e0f7e1609be0
Alan Cutter <alancutter@chromium.org>
2016-10-13 16:45:51 +1100

    crbug

--- client/crbug-lib.js
+++ client/crbug-lib.js
@@ -1,6 +1,6 @@
 (function() {
 
-if (window.Monorail) {
+if (window.Crbug) {
   return;
 }
 
@@ -12,7 +12,7 @@ function queryURL(query) {
   return 'https://bugs.chromium.org/p/chromium/issues/list?can=2&q=' + encodeURIComponent(queryString(query));
 }
 
-window.Monorail = {
+window.Crbug = {
   queryString,
   queryURL,
 };
