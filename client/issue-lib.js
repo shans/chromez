96f051b418d4bc2b361b7b2976bd05d61e339402
Alan Cutter <alancutter@chromium.org>
2016-10-13 12:03:42 +1100

    Reword comment

--- client/issue-lib.js
+++ client/issue-lib.js
@@ -45,8 +45,8 @@ var _defaultReviewLevel = 'none';
 var _inSLOColor = '#4CAF50';  // Green 500
 var _noSLOColor = '#757575';  // Grey 600
 
-// This class uses C style programming on JSON objects (representing issues) and
-// static functions.
+// This class uses C style programming with JSON objects (representing issues)
+// and static functions.
 // This is to maintain the ability to clone the object in chromez-behaviours.js.
 class Issue {
   static create({id, owner, summary, lastUpdatedString, labels}) {
