f64026063849e79a84e4e10d3b8328c4602251fa
Alan Cutter <alancutter@chromium.org>
2016-10-18 11:47:36 +1100

    Don't return source element

--- client/issues-lib.js
+++ client/issues-lib.js
@@ -76,7 +76,7 @@ class Issue {
   clone() {
     var params = {};
     Object.assign(params, this);
-    params.labels = clone(labels);
+    params.labels = clone(this.labels);
     return new Issue(params);
   }
 
@@ -94,15 +94,20 @@ class Issue {
 }
 
 class IssueList {
-  constructor(issues = []) {
+  constructor(issues = [], getQueryURL = null) {
     for (var issue of issues) {
       console.assert(issue instanceof Issue);
     }
     this._issues = issues;
+    this._getQueryURL = getQueryURL;
+  }
+
+  getQueryURL(subQuery) {
+    return this._getQueryURL(subQuery);
   }
 
   clone() {
-    return new IssueList(this._issues.map(issue => issue.clone()));
+    return new IssueList(this._issues.map(issue => issue.clone()), this._getQueryURL);
   }
 
   push(issue) {
